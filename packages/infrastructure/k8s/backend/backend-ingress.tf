175b45ea87faff829d93337e2af0fc68c9db05f6
Loïc VDB <l.vandenbossche.jouvet@gmail.com>
2026-02-12 00:59:26 +0100

    try deploy front

--- packages/infrastructure/k8s/backend/backend-ingress.tf
+++ packages/infrastructure/k8s/backend/backend-ingress.tf
@@ -4,8 +4,6 @@ resource "kubernetes_ingress_v1" "hollybike_backend_ingress" {
     namespace = var.namespace
     annotations = {
       "cert-manager.io/cluster-issuer"                 = "letsencrypt-production",
-      "acme.cert-manager.io/http01-edit-in-place"      = "true",
-      "acme.cert-manager.io/http01-ingress-class"      = "nginx",
       "nginx.ingress.kubernetes.io/ssl-redirect"       = "true",
       "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true",
       "nginx.ingress.kubernetes.io/backend-protocol"   = "HTTP",
--- packages/infrastructure/k8s/frontend/frontend-ingress.tf
+++ packages/infrastructure/k8s/frontend/frontend-ingress.tf
@@ -102,8 +102,6 @@ resource "kubernetes_ingress_v1" "hollybike_frontend_spa" {
     namespace = var.namespace
     annotations = {
       "cert-manager.io/cluster-issuer"                 = "letsencrypt-production"
-      "acme.cert-manager.io/http01-edit-in-place"      = "true"
-      "acme.cert-manager.io/http01-ingress-class"      = "nginx"
       "nginx.ingress.kubernetes.io/ssl-redirect"       = "true"
       "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
       "nginx.ingress.kubernetes.io/backend-protocol"   = "HTTP"
