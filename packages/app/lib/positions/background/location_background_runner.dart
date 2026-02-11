e17861f408b02b61ce506f4911714abf686dedef
Loïc VDB <l.vandenbossche.jouvet@gmail.com>
2026-02-11 21:46:47 +0100

    fix: updated distance filter to 10 meters

--- packages/app/lib/positions/background/location_background_runner.dart
+++ packages/app/lib/positions/background/location_background_runner.dart
@@ -52,7 +52,7 @@ class LocationBackgroundRunner {
 
     final settings = AndroidSettings(
       accuracy: LocationAccuracy.best,
-      distanceFilter: 0,
+      distanceFilter: 10,
       intervalDuration: Duration(seconds: 5),
     );
 
