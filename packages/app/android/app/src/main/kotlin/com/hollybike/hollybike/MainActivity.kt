4184c32e81033d0d7ad9b0542a59a2d41194edaa
Loïc VDB <l.vandenbossche.jouvet@gmail.com>
2026-02-14 18:16:38 +0100

    fix: fixed permissions & warns

--- packages/app/android/app/src/main/kotlin/com/hollybike/hollybike/MainActivity.kt
+++ packages/app/android/app/src/main/kotlin/com/hollybike/hollybike/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.ForegroundServiceStartNotAllowedException
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.hollybike.hollybike.realtime.RealtimeForegroundService
 import com.hollybike.hollybike.tracking.TrackingForegroundService
 import io.flutter.embedding.android.FlutterActivity
@@ -20,6 +22,11 @@ class MainActivity : FlutterActivity() {
 	companion object {
 		@JvmStatic
 		var uiMessenger: BinaryMessenger? = null
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		WindowCompat.setDecorFitsSystemWindows(window, false)
 	}
 
 	override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
--- packages/app/lib/image/widgets/image_picker/image_picker_choice_list.dart
+++ packages/app/lib/image/widgets/image_picker/image_picker_choice_list.dart
@@ -2,10 +2,15 @@
   Hollybike Mobile Flutter application
   Made by enzoSoa (Enzo SOARES) and Loïc Vanden Bossche
 */
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hollybike/image/type/image_picker_mode.dart';
 import 'package:hollybike/image/utils/image_picker/img.dart';
 import 'package:hollybike/image/widgets/image_picker/image_picker_gallery_button.dart';
+import 'package:hollybike/image/widgets/image_picker/image_picker_thumbnail.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 import 'image_picker_camera_button.dart';
 
@@ -28,7 +33,28 @@ class ImagePickerChoiceList extends StatefulWidget {
 }
 
 class _ImagePickerChoiceListState extends State<ImagePickerChoiceList> {
-  bool get isLoading => widget.isLoading;
+  final assetEntitiesList = <AssetEntity>[];
+  bool _loadingImages = true;
+  final imagePicker = ImagePicker();
+
+  bool get isLoading => _loadingImages || widget.isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkImagesPermission()
+        .then((granted) {
+      if (granted) {
+        return _loadImages();
+      }
+    })
+        .whenComplete(() {
+      setState(() {
+        _loadingImages = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +62,13 @@ class _ImagePickerChoiceListState extends State<ImagePickerChoiceList> {
       ImagePickerCameraButton(
         onImageSelected: (image) => widget.onImagesSelected([image]),
       ),
+      ...assetEntitiesList.map((entity) {
+        return ImagePickerThumbnail(
+          assetEntity: entity,
+          isSelected: widget.entityIdSelectedList.contains(entity.id),
+          onImageSelected: (image) => widget.onImagesSelected([image]),
+        );
+      }),
       ImagePickerGalleryButton(
         mode: widget.mode,
         onImagesSelected: widget.onImagesSelected,
@@ -46,7 +79,7 @@ class _ImagePickerChoiceListState extends State<ImagePickerChoiceList> {
 
     return AnimatedCrossFade(
       crossFadeState:
-          isLoading ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      isLoading ? CrossFadeState.showFirst : CrossFadeState.showSecond,
       duration: const Duration(milliseconds: 200),
       firstChild: SizedBox(
         height: height,
@@ -71,5 +104,58 @@ class _ImagePickerChoiceListState extends State<ImagePickerChoiceList> {
         ),
       ),
     );
+  }
+
+  void _loadImages() async {
+    final filterOption = FilterOptionGroup(
+      orders: const [
+        OrderOption(type: OrderOptionType.updateDate, asc: false),
+        OrderOption(type: OrderOptionType.createDate, asc: false),
+      ],
+    );
+
+    final List<AssetPathEntity> paths = await PhotoManager.getAssetPathList(
+      onlyAll: true,
+      type: RequestType.image,
+      filterOption: filterOption,
+    );
+
+    if (paths.isEmpty) {
+      return;
+    }
+
+    final path = await paths.first.obtainForNewProperties();
+
+    final List<AssetEntity> entities = await path.getAssetListPaged(
+      page: 0,
+      size: 10,
+    );
+
+    setState(() {
+      assetEntitiesList.addAll(entities);
+    });
+  }
+
+  Future<bool> _checkImagesPermission() async {
+    final permission = await PhotoManager.requestPermissionExtend(
+      requestOption: const PermissionRequestOption(
+        androidPermission: AndroidPermission(
+          type: RequestType.image,
+          mediaLocation: true,
+        ),
+      ),
+    );
+
+    if (permission.hasAccess) {
+      return true;
+    }
+
+    if (Platform.isIOS || Platform.isMacOS) {
+      await PhotoManager.presentLimited(type: RequestType.image);
+    } else {
+      await PhotoManager.openSetting();
+    }
+
+    return false;
   }
 }
