339d3da245c4d8b4dbfa1db731b1a4feead02909
Loïc VDB <l.vandenbossche.jouvet@gmail.com>
2026-02-16 22:37:30 +0100

    fix: fixed add image dont works from event images

--- packages/app/lib/event/widgets/images/show_event_images_picker.dart
+++ packages/app/lib/event/widgets/images/show_event_images_picker.dart
@@ -7,13 +7,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hollybike/event/bloc/event_images_bloc/event_my_images_bloc.dart';
 import 'package:hollybike/event/widgets/images/event_image_picker_modal.dart';
 
-void showEventImagesPicker(BuildContext context, int eventId) {
+void showEventImagesPicker(
+  BuildContext context,
+  int eventId, {
+  EventMyImagesBloc? bloc,
+}) {
+  final eventMyImagesBloc = bloc ?? context.read<EventMyImagesBloc>();
+
   showModalBottomSheet<void>(
     context: context,
     backgroundColor: Colors.transparent,
     builder: (_) {
       return BlocProvider.value(
-        value: context.read<EventMyImagesBloc>(),
+        value: eventMyImagesBloc,
         child: const EventImagePickerModal(),
       );
     },
